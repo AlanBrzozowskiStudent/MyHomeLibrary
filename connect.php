aebc1fe94b91c91102c699753e7f119a7fdd255c
Lewapek03 <pawartb@wp.pl>
2024-01-11 20:59:24 +0100

    Fix connect and add docker files

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
     // jeden plik do łączenia z DB, dane do logowania w jednym miejscu
-    $host = "localhost";
-    $db_user = "root";
-    $db_password = "";
-    $db_name = "projekt";
+    $host = "185.224.137.4";
+    $db_user = "u791771945_admin";
+    $db_password = ",32X-]]7K4yH%nfB?M!=%e=t";
+    $db_name = "u791771945_23tfa3634";
 ?>
