6997fb9f7a605493c0e16e6c11ca1e1f6861475b
Alan Brzozowski <brzozowskialan23@gmail.com>
2024-01-12 19:35:30 +0100

    fix login handler

--- login_handler.php
+++ login_handler.php
@@ -13,8 +13,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $connection->real_escape_string($_POST['email']);
     $password = $connection->real_escape_string($_POST['password']);
 
-    $stmt = $connection->prepare("SELECT id, name, password FROM users WHERE email = 'pawartb@wp.pl'");
+    $stmt = $connection->prepare("SELECT id, name, email, password FROM users WHERE email = ?");
+    $stmt->bind_param("s", $email);
     $stmt->execute();
+
     $result = $stmt->get_result();
 
     if ($result->num_rows === 1) {
@@ -22,8 +24,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (password_verify($password, $user['password'])) {
             $_SESSION['id'] = $user['id'];
             $_SESSION['name'] = $user['name'];
-            $_SESSION['zalogowany'] = true;
             $_SESSION['email'] = $user['email'];
+            $_SESSION['zalogowany'] = true;
 
             header('Location: index.php');
         } else {
