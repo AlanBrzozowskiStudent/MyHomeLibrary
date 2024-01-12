e3ea3531e60f6d6dd2898c4b9f541a4d188d13b9
Alan Brzozowski <brzozowskialan23@gmail.com>
2024-01-12 15:45:42 +0100

    add new look xd

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MyHomeLibrary - Home</title>
-    <link rel="shortcut icon" type="image/ico" href="./img/Paomedia-Small-N-Flat-Book.png">
+    <title>MyNewHome - Welcome Page</title>
+    <link rel="shortcut icon" type="image/ico" href="./img/house_icon.png">
     <!-- Importy fonty, bootstrap, ikony, css -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -19,7 +23,7 @@
     <!-- Navbar z bootstrapa -->
     <nav class="navbar navbar-expand-lg py-2 sticky-top" style="background-color: #1F8A70;">
         <div class="container">
-          <a class="navbar-brand text-light" href="#"><i class="bi bi-book text-light"></i> MyHomeLibrary</a>
+          <a class="navbar-brand text-light" href="#"><i class="bi bi-house-heart text-light"></i> MyNewHome</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -27,10 +31,9 @@
             <div class="navbar-nav ms-auto">
               <a class="nav-link px-lg-3 text-light rounded-pill" href="index.php">Home</a>
                 <?php
-                  session_start();
                   if ((isset($_SESSION['zalogowany'])) && ($_SESSION['zalogowany']==true)) // jeśli user jest zalogowany pokaż Twoje konto
                   {
-                    echo '<a class="nav-link px-lg-3 text-light rounded-pill" href="library.php">Twoja Biblioteka</a>';
+                    echo '<a class="nav-link px-lg-3 text-light rounded-pill" href="page_offers.php">Oferty</a>';
                     echo '<a class="nav-link px-lg-3 text-light rounded-pill" href="main.php">Twoje konto</a>';
                     echo '<a class="nav-link px-lg-3 text-light rounded-pill" href="logout.php">Wyloguj</a>';
                   }
@@ -52,8 +55,8 @@
     </section>
 
     <section class="welcome-text d-flex flex-column justify-content-center align-items-center text-center">
-      <p>Twoja aplikacja do zarządzania domową biblioteką!</p>
-      <p class="text-how-it-work">Jak to działa? zmianaaaaaassssssssssssqqqqqqqqqqqtttt</p>
+      <h4>Czy szukasz idealnej przestrzeni, aby rozpocząć nowy rozdział w swoim życiu?</h4>
+      <p class="text-how-it-work">Odkryj prawdziwe miejsce do życia z ofertami domów na MyNewHome!</p>
     </section>
 
     <section class="how-works-section">
@@ -61,26 +64,25 @@
         <div class="col-12 col-md-6 col-lg-3 ">
             <div class="how-it-works m-3 p-4 p-lg-5 border text-dark">
             <h3>1</h3>
-            <p>Utwórz konto aby zapisywać swój postęp! </p>
+            <p>Utwórz konto aby mieć dostęp do ofert! </p>
             </div>
         </div>
         <div class="col-12 col-md-6 col-lg-3">
             <div class="how-it-works m-3 p-4 p-lg-5 border text-dark">
             <h3>2</h3>
-            <p>Dodaj swoją kolekcję książek do konta</p>
+            <p>Przeglądaj oferty nieruchomości.</p>
             </div>
         </div>
         <div class="col-12 col-md-6 col-lg-3">
             <div class="how-it-works m-3 p-4 p-lg-5 border text-dark ">
             <h3>3</h3>
-            <p>Przeczytaj, oceń, zapisz ulubione cytaty</p>
+            <p>Znajdź wymarzoną nieruchomość.</p>
             </div>
         </div>
         <div class="col-12 col-md-6 col-lg-3">
             <div class="how-it-works m-3 p-4 p-lg-5 border text-dark">
             <h3>4</h3>
-            <p>Wybierz następną pozycję z MyHomeLibrary!</p>
-            </div>
+            <p>Kup i zacznij nowe życie dzięki MyNewHome!</p>
         </div>
       </div>
     </section>
@@ -88,54 +90,55 @@
   <section id="example" class="example">
         <!-- sekcja card z bootstrapa -->
         <div class="container py-5 text-center">
-            <p class="display-3 orange-color mb-0 text-uppercase">Przykład</p>
-            <p class="py-3">Twojej domowej kolekcji!</p>
+            <p class="display-3 orange-color mb-0 text-uppercase">Przykładowe oferty:</p>
             <div class="row row-cols-1 row-cols-md-2 row-cols-xl-4">
                 <div class="col">
                   <div class="card h-100">
-                    <img src="./img/quo-vadis.png" class="card-img-top" alt="Okladka książki Quo Vadis">
+                    <img src="./img/example_photo2.jpg" class="card-img-top" alt="Oferta domu 1">
                     <div class="card-body">
-                      <h5 class="card-title">Quo Vadis</h5>
-                      <p class="card-text mb-0">Autor:Henryk Sienkiewicz</p>
-                      <p class="card-text">Ocena:8/10</p>
+                      <h5 class="card-title">Poznań Jeżyce</h5>
+                      <p class="card-text mb-0">65m2</p>
+                      <p class="card-text">750 000zł</p>
                     </div>
                   </div>
                 </div>
                 <div class="col">
                   <div class="card h-100">
-                    <img src="./img/html5-i-css3-definicja-nowoczesnosci.png" class="card-img-top" alt="Okladka książki HTML5 i CSS3. Definicja nowoczesności">
+                    <img src="./img/example_photo3.jpg" class="card-img-top" alt="Oferta domu 2">
                     <div class="card-body">
-                      <h5 class="card-title">Definicja nowoczesności</h5>
-                      <p class="card-text mb-0">Autor:Mazur Dawid</p>
-                      <p class="card-text">Ocena:6/10</p>
+                      <h5 class="card-title">Poznań Wilda</h5>
+                      <p class="card-text mb-0">35m2</p>
+                      <p class="card-text">550 000zł</p>
                     </div>
                   </div>
                 </div>
                 <div class="col">
                   <div class="card h-100">
-                    <img src="./img/atomic-habits.png" class="card-img-top" alt="Okladka książki Atomic Habits">
+                    <img src="./img/example_photo4.jpg" class="card-img-top" alt="Oferta domu 3">
                     <div class="card-body">
-                      <h5 class="card-title">Atomic Habits</h5>
-                      <p class="card-text mb-0">Autor:Clear James</p>
-                      <p class="card-text">Ocena:10/10</p>
+                      <h5 class="card-title">Poznań Centrum</h5>
+                      <p class="card-text mb-0">75m2</p>
+                      <p class="card-text">900 000zł</p>
                     </div>
                   </div>
                 </div>
                 <div class="col">
                   <div class="card h-100">
-                    <img src="./img/21-lekcji.png" class="card-img-top" alt="Okladka książki 21 lekcji na XXI wiek">
+                    <img src="./img/example_photo5.jpg" class="card-img-top" alt="Oferta domu 4">
                     <div class="card-body">
-                      <h5 class="card-title">21 lekcji na XXI wiek</h5>
-                      <p class="card-text mb-0">Autor:Harari Yuval Noah</p>
-                      <p class="card-text">Ocena:8/10</p>
+                      <h5 class="card-title">Luboń</h5>
+                      <p class="card-text mb-0">135m2</p>
+                      <p class="card-text">950 000zł</p>
                     </div>
                   </div>
                 </div>
+
+
               </div>
         </div>
     </section>
   <footer class="bg-dark text-light border-top">
-      <p class="text-center mb-0 py-5">&copy; MyHomeLibrary 2023</p>
+      <p class="text-center mb-0 py-5">&copy; MyNewHome 2024</p>
   </footer>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
--- main.php
+++ main.php
@@ -13,8 +13,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MyHomeLibrary - Twoje konto</title>
-    <link rel="shortcut icon" type="image/ico" href="./img/Paomedia-Small-N-Flat-Book.png">
+    <title>MyNewHome - Your account</title>
+    <link rel="shortcut icon" type="image/ico" href="./img/house_icon.png">
     <!-- Importy fonty, bootstrap, ikony, css -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -28,7 +28,7 @@
     <!-- Navbar z bootstrapa -->
     <nav class="navbar navbar-expand-lg py-2 sticky-top" style="background-color: #1F8A70;">
         <div class="container">
-          <a class="navbar-brand text-light" href="#"><i class="bi bi-book text-light"></i> MyHomeLibrary</a>
+          <a class="navbar-brand text-light" href="#"><i class="bi bi-house-heart text-light"></i> MyNewHome</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -38,7 +38,7 @@
               <?php
                 if ((isset($_SESSION['zalogowany'])) && ($_SESSION['zalogowany']==true)) // jeśli user jest zalogowany pokaż Twoje konto
                 {
-                  echo '<a class="nav-link px-lg-3 text-light rounded-pill" href="library.php">Twoja Biblioteka</a>';
+                    echo '<a class="nav-link px-lg-3 text-light rounded-pill" href="page_offers.php">Oferty</a>';
                   echo '<a class="nav-link px-lg-3 text-light rounded-pill" href="main.php">Twoje konto</a>';
                   echo '<a class="nav-link px-lg-3 text-light rounded-pill" href="logout.php">Wyloguj</a>';
                 }
@@ -76,7 +76,7 @@
     ?>
 
   <footer class="bg-dark text-light border-top">
-    <p class="text-center mb-0 py-5">&copy; MyHomeLibrary 2023</p>
+      <p class="text-center mb-0 py-5">&copy; MyNewHome 2024</p>
   </footer>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
